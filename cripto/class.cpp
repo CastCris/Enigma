eca76c51f323007e8f9c8ad82a1f90b23ca3f902
Cristiano <cristianochincarosanches@gmail.com>
2024-07-09 08:40:15 -0300

    debug

--- cripto/class.cpp
+++ cripto/class.cpp
@@ -1,3 +1,7 @@
+/*
+Para cada caso de teste, não é permitido colocar um número maior que 88, senão ele será subtraido até dar um número menor que 80.
+O limite do tamanho da mensagem é ilimitado, porém, algumas palavras podem vim erradas, mas não o suficiente para deixar o texto ilégivel
+*/
 #include <iostream>
 #include <map>
 #include <vector>
@@ -16,23 +20,29 @@ class enigma{
                 cara=j;
                 rotor();
                 ++mudan[0];
-                if(mudan[0]>126){
-                    mudan[0]-=126;
+                if(mudan[0]>88){
+                    mudan[0]-=89*int(mudan[0]/89);
+                    mudan[1]+=1;
                     for(int i=1;i<mudan.size();++i){
-                        if(mudan[i]>126){
-                            mudan[i]-=126*int(mudan[i]/126);
+                        if(mudan[i]>89){
+                            mudan[i]-=89*int(mudan[i]/89);
+                            mudan[i]+=1;
                         }else{
                             break;
                         }
                     }
                 }
+                std::cout<<j<<" "<<mudan[0]<<" "<<mudan[1]<<" "<<mudan[2]<<std::endl;
                 for(int i=0;i<r.size();++i){
                     cara=r[i][cara];
+                    std::cout<<cara<<" ";
                 }
-                for(int i=r.size()-1;i>-1;--i){
+                for(int i=r.size()-1;i>=0;--i){
                     cara=r[i][cara];
+                    std::cout<<cara<<" ";
                 }
-                output.push_back(cara);
+                std::cout<<std::endl;
+                (int(cara)<33|int(cara)>121)?output.push_back('!'):output.push_back(cara);
                 r.clear();
             }
             return output;
@@ -40,49 +50,46 @@ class enigma{
     private:
         std::vector<std::map<char, char>> r;
         void rotor(){
-            int splep=0, v1=0,v2=0, dis_char;
+            int splep=0, v1=0,v2=0, loc;
             for(int index=0;index<mudan.size();index++){
                 r.push_back({});
-                for(int letra=33;letra<126;++letra){
-                    splep=letra;
-                    splep+=mudan[index];
-                    if(splep>126){
-                        splep=splep-126*int(splep/93)+32;
-                    }
+                for(int letra=0;letra<89;++letra){
+                    splep=mudan[index]+33+letra;
+                    (splep>121)?splep-=89*int(splep/121):splep;
                     if(criptar){
-                        v1=letra;
+                        v1=letra+33;
                         v2=splep;
                     } else{
                         v1=splep;
-                        v2=letra;
+                        v2=letra+33;
                     }
                     r[index].insert(std::pair<char, char>{char(v1),char(v2)});
                 }
             }
         }
 };
-// 
-int main(){
-    enigma trem;
-    std::string coiso;
-    int bo;
-    while(true){
-        std::cin>>trem.word;
-        // 
-        std::cin.clear();
-        std::cin.ignore(INT_MAX,'\n');
-        // 
-        std::getline(std::cin,coiso);
-        char*split=std::strtok(&coiso[0]," ");
-        for(int i=0;split!=NULL;++i){
-            trem.mudan.push_back(std::stoi(split));
-            split=std::strtok(NULL," ");
-        }
-        std::cout<<"-----------"<<std::endl;
-        std::cin>>bo;
-        (bo==1)?trem.criptar=true:trem.criptar=false;
-        std::cout<<trem.cifra()<<std::endl;
-        trem.mudan.clear();
-    }
-    return 0;
-}
+// Caso de teste
+// int main(){
+//     enigma trem;
+//     std::string coiso;
+//     int bo;
+//     while(true){
+//         std::cin>>trem.word;//ESTAMOS-VIVENDO-UM-APOCALIPSE-ZUMBI!
+//         // 
+//         std::cin.clear();
+//         std::cin.ignore(INT_MAX,'\n');
+//         // 
+//         std::getline(std::cin,coiso);
+//         char*split=std::strtok(&coiso[0]," ");
+//         for(int i=0;split!=NULL;++i){
+//             trem.mudan.push_back(std::stoi(split));
+//             split=std::strtok(NULL," ");
+//         }
+//         std::cout<<"-----------"<<std::endl;
+//         std::cin>>bo;
+//         (bo==1)?trem.criptar=true:trem.criptar=false;
+//         std::cout<<trem.cifra()<<std::endl;
+//         trem.mudan.clear();
+//     }
+//     return 0;
+// }
