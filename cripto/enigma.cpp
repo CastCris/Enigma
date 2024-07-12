23441980a56f1b9a18b9bc5825e1c13fd51cf797
Cristiano <cristianochincarosanches@gmail.com>
2024-07-12 19:44:26 -0300

    plugin--otimizacao

--- cripto/enigma.cpp
+++ cripto/enigma.cpp
@@ -7,6 +7,10 @@ O limite do tamanho da mensagem é ilimitado, porém, algumas palavras podem vim
 #include <vector>
 #include <cstring>
 // 
+#include <fstream>
+#include <unistd.h>
+#include <sys/stat.h>
+// 
 class enigma{
     public:
         bool criptar;
@@ -14,14 +18,19 @@ class enigma{
         std::vector<int> mudan;
         std::string misterio(){
             plu();
-            return cifra(mudan,word,plus);
+            auto retornar= cifra(mudan,word,*redire);
+            delete redire;
+            redire=NULL;
+            redire = new std::map<char,char>;
+            return retornar;
         }
     private:
         std::vector<std::map<char, char>> r;
-        std::map<char, char> plus;
-        void rotor(std::vector<int> giragira){
+        std::map<char, char>* redire = new std::map<char,char>;
+        // 
+        void rotor(std::vector<int> giragira,int max){
             int splep=0, v1=0,v2=0;
-            for(int index=0;index<giragira.size();index++){
+            for(int index=0;index<max;index++){
                 r.push_back({});
                 for(int letra=0;letra<89;++letra){
                     splep=giragira[index]+33+letra;
@@ -43,22 +52,24 @@ class enigma{
             while(dividir!=NULL){
                 temp=dividir;
                 if(temp.length()==3){
-                    (criptar)?plus[temp[0]]=temp[2]:plus[temp[2]]=temp[0];
+                    redire->operator[](temp[0])=temp[2];
                 }
                 dividir=std::strtok(NULL," ");
             }
             return 0;
         }
-        std::string cifra(std::vector<int> girar, std::string pla, std::map<char,char> gin){
+        std::string cifra(std::vector<int> girar, std::string letter, std::map<char,char> gin){
             std::string output="";
             char cara;
-            for(char j:pla){
-                cara=j;
-                rotor(girar);
+            int quant=girar.size();
+            for(char ll:letter){
+                cara=ll;
+                rotor(girar,quant);
                 ++girar[0];
                 if(girar[0]>88){
                     girar[0]-=88*int(girar[0]/88);
                     girar[1]+=1;
+                    ++quant;
                     for(int i=1;i<girar.size();++i){
                         if(girar[i]>89){
                             girar[i]-=89*int(girar[i]/89);
@@ -66,52 +77,68 @@ class enigma{
                         }else{
                             break;
                         }
+                        ++quant;
                     }
+                } else{
+                    quant=1;
                 }
                 for(int emb=0;emb<r.size();++emb){
-                    if(!criptar&&int(girar[cara])>32&&int(girar[cara])<122){
-                        cara=girar[cara];
+                    if(!criptar){
+                        if(gin.find(cara)!=gin.end()){
+                            cara=gin[cara];
+                        } else{
+                            for(auto andar:gin){
+                                if(andar.second==cara){
+                                    cara=andar.first;
+                                    break;
+                                }
+                            }
+                        }
                     }
                     cara=r[emb][cara];
-                    if(!criptar&&int(girar[cara])>32&&int(girar[cara])<122){
-                        cara=girar[cara];
+                    if(criptar){
+                        if(gin.find(cara)!=gin.end()){
+                            cara=gin[cara];
+                        } else{
+                            for(auto andar:gin){
+                                if(andar.second==cara){
+                                    cara=andar.first;
+                                    break;
+                                }
+                            }
+                        }
                     }
                 }
                 for(int emb=r.size()-1;emb>-1;--emb){
+                    if(!criptar){
+                        if(gin.find(cara)!=gin.end()){
+                            cara=gin[cara];
+                        } else{
+                            for(auto andar:gin){
+                                if(andar.second==cara){
+                                    cara=andar.first;
+                                    break;
+                                }
+                            }
+                        }
+                    }
                     cara=r[emb][cara];
-                    std::cout<<cara<<std::endl;
+                    if(criptar){
+                        if(gin.find(cara)!=gin.end()){
+                            cara=gin[cara];
+                        } else{
+                            for(auto andar:gin){
+                                if(andar.second==cara){
+                                    cara=andar.first;
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 }
                 (int(cara)<33|int(cara)>121)?output.push_back('!'):output.push_back(cara);
                 r.clear();
             }
-            plus.clear();
             return output;
         };
 };
-// Caso de teste
-int main(){
-    enigma trem;
-    std::string coiso;
-    int bo;
-    while(true){
-        std::cin>>trem.word;
-        // 
-        std::cin.clear();
-        std::cin.ignore(INT_MAX,'\n');
-        // 
-        std::getline(std::cin,coiso);
-        char*split=std::strtok(&coiso[0]," ");
-        for(int i=0;split!=NULL;++i){
-            trem.mudan.push_back(std::stoi(split));
-            split=std::strtok(NULL," ");
-        }
-        std::cout<<"-----------"<<std::endl;
-        std::getline(std::cin,trem.plugin);
-        std::cout<<"-----------"<<std::endl;
-        std::cin>>bo;
-        (bo==1)?trem.criptar=true:trem.criptar=false;
-        std::cout<<trem.misterio()<<std::endl;
-        trem.mudan.clear();
-    }
-    return 0;
-}
