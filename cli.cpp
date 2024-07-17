2059013866a83ddf6c712e1942cc7fe4b5c33de7
Cristiano <cristianochincarosanches@gmail.com>
2024-07-16 23:46:20 -0300

    cli--finish--

--- cli.cpp
+++ cli.cpp
@@ -15,52 +15,52 @@ int main(){
     std::cout<<"$$ |_____ $$ |  $$ |$$ |$$ |__$$ |$$ | $$ | $$ |/$$$$$$$ |"<<std::endl;
     std::cout<<"$$       |$$ |  $$ |$$ |$$    $$ |$$ | $$ | $$ |$$    $$ |"<<std::endl;
     std::cout<<"$$$$$$$$/ $$/   $$/ $$/  $$$$$$$ |$$/  $$/  $$/  $$$$$$$/ "<<std::endl;
-    std::cout<<"                        /  /__$$ |                        "<<std::endl;
-    std::cout<<"                         $$    $$/                         "<<std::endl;
+    std::cout<<"                         / /__$$ |                        "<<std::endl;
+    std::cout<<"                         $$    $$/                        "<<std::endl;
     std::cout<<"                         $$$$$$/                          "<<std::endl;
     std::cout<<"\nPara mais informações, digite info\nPara ascessar o manual, digite m\nPara iniciar a Enigma, digite e\nPara obter ajuda com os comandos, digite h seguido pelo comando\nPor fim, para finalizar o processo, digite p\n";
     while(true){
-        std::cin>>inp;
+        std::cout<<"* ";
+        std::getline(std::cin,inp);
         if(inp=="info"){
-            std::cout<<"BOB";
+            std::cout<<"BOB"<<std::endl;
         } else if(inp=="m"|inp=="M"){
             std::cout<<"-------Manual-------\nEscolha algum dos items:\n1-Manipulação do histórico\n2-Entrada de valores\n";
             inp.clear();
             std::cout<<"*-M ";
             std::cin>>inp;
             if(inp=="1"){
-                std::cout<<"-------Manipulação do histórico-------\nDurante a execução do programa, o algoritimo salva os seus dados de forma que, após o termino do programa, estes possam ser salvos em um arquivo .txt. Para ascessar o histórico local, isto é, aquele que ainda não foi gravado, basta utilizar o comando: hl. Caso contrário, para ascessar o histórico global use hg\nCom o ascesso à algum dos históricos acima, é possível editá-los e/ou visualiza-lós através dos seguintes comandos:\nv->Visualiza o conteúdo do histórico\nex->Exclui uma ou mais linhas desejadas\ni->insere elementos no histórico\n\nNota: Caso o programa seja forçado a parar ou ocorra erros durante a sua execução, os dados salvos localmente serão perdidos\n";
+                std::cout<<"-------Manipulação do histórico-------\nDurante a execução do programa, o algoritimo salva os seus dados de forma que, após o termino do programa, estes possam ser salvos em um arquivo .txt. Para visualizar o histórico, utilize o comando vi, caso quira procurar informações no histórico, utiliza o comando procurar seguido pelo item de busca(Horário, texto, rotor(es),plugin(s) e modo)\n"<<std::endl;
             } else if(inp=="2"){
-                std::cout<<"-------Entrada-------\nA primeira entrada é composta pela mensagem a ser criptograda/descriptograda. Os caracteres devem ter valores >32 e <123 da tabela ASCII exeto pelo espaço.\nA segunda entrada consiste nos plugins. Para cada caracter não pode haver um que se repita mais de 1 vez, obedecem à primeira regra de valores ASCII e devem se interligados à um outro caracter por um hífen\nA terceira entrada consiste nos rotores. Os valores dos rotores não podem ser <0 e >89, cada espaço entre os valores, será interpretado como um rotor distinto\nA última entrada consiste no modo(0/1). Para uma mensagem criptografada no modo 1, a mensagem deve ser descriptograda no modo 0 e vice-versa.\n";
+                std::cout<<"-------Entrada-------\nA primeira entrada é composta pela mensagem a ser criptograda/descriptograda. Os caracteres devem ter valores >32 e <123 e !=95('_') da tabela ASCII exeto pelo espaço.\nA segunda entrada consiste nos plugins. Para cada caracter não pode haver um que se repita mais de 1 vez, obedecem à primeira regra de valores ASCII e devem se interligados à um outro caracter por um hífen\nA terceira entrada consiste nos rotores. Os valores dos rotores não podem ser <0 e >89, cada espaço entre os valores, será interpretado como um rotor distinto\nA última entrada consiste no modo(0/1). Para uma mensagem criptografada no modo 1, a mensagem deve ser descriptograda no modo 0 e vice-versa.\n"<<std::endl;
             } else{
-                std::cout<<"Inválido, tente novamente";
+                std::cout<<"Inválido, tente novamente"<<std::endl;
             }
         } else if(inp[0]=='h'|inp[0]=='H'){
-            dividir=std::strtok(&inp[0],NULL);
-            for(int i=0;dividir!=NULL;++i){
-                (i>0)?help=std::to_string(*dividir):help;
-            }
-            if(help=="v"){
-                out="v->Visualiza o histórico selecionado\nEX: *hl: v\n########################Sun Jul 14 22:59:59 2024\nTexto: CFI\nRotor: 1\nPlugin:(nada)\nModo: 0\n";
-            } else if(help=="hl"){
-                // A escrever
-            } else if(help=="hg"){
-                // A escrever
-            } else if(help=="m"|help=="M"){
-                // A escrever
-            } else if(help=="ex"){
-                // A escrever
-            } else if(help=="i"){
-                // A escreber
-            } else if(help=="e"|help=="E"){
-                // A escrever
-            } else if(help=="i"|help=="I"){
-                // A escrever
-            } else{
-                out="Tente novamente";
+            dividir=std::strtok(&inp[0]," ");
+            dividir=std::strtok(NULL," ");
+            std::cout<<dividir<<std::endl;
+            help=dividir;
+            if(help=="vi"){
+                out="vi->Visualiza o históric\nEX: *: v\n########################Sun Jul 14 22:59:59 2024\nTexto: CFI\nRotor: 1\nPlugin:(nada)\nModo: 0\n";
+            } else if(help=="proc"){
+                out="proc->Após a sua inserção, digite um ous mais parâmetros a serem pesquisado no histório\nH|h->Horário\nT|t->Texto\nR|r->Rotor\nP|p->plugin\nM|m->modo";
+            } else if(help=="e"){
+                out="e|E->Inicia a Enigma até o input texto for igual à p.Após o seu termino, o histórico é automaticamente salvo em um arquivo .txt";
+            } else if(help=="p"|help=="P"){
+                out="p|P->Interrompe a operação da interação";
+            }else{
+                out="Inválido, tente novamente";
             }
             std::cout<<out<<std::endl;
             out.clear();
+            help.clear();
+        } else if(inp=="e"|inp=="E"){
+
+        }else if(inp=="p"|inp=="P"){
+            break;
+        } else{
+            std::cout<<"Inválido, tente novamente"<<std::endl;
         }
     }
     inp.clear();
